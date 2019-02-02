1c0df45c717fecd521c67b2d799bf8a670d73bc5
unknown <hellen.kwon@gmail.com>
2019-02-02 14:30:26 -0800

    Updated the code for the lif 02/02//2019 JK

---
+++ 2019 Deep Space/src/main/java/frc/Mechanisms/CatzLift.java
@@ -0,0 +1,84 @@
+package frc.Mechanisms;
+import com.revrobotics.CANDigitalInput;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+
+//import edu.wpi.first.wpilibj.Encoder;
+//import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+
+/*
+ *  Author : Jean Kwon
+ *  
+ *  Revision History : 
+ *  02-01-19 Added the method setLiftHeight JK
+ *  Methods : lift(controll the lift), get LiftCounts, isLiftLimitSwitchTop, is LiftLimitSwitchBot, setLiftHeight
+ *  Functionality : Code for the lift  
+ * 
+ */
+public class CatzLift
+{
+    private static CANSparkMax liftMtrCtrlLT;
+    private static CANSparkMax liftMtrCtrlRT;
+    
+    private static SpeedControllerGroup liftMotors;
+
+    private static final int LIFT_RT_MC_CAN_ID = 11; 
+    private static final int LIFT_LT_MC_CAN_ID = 10;
+
+    /*public static Encoder liftEnc;              
+    private static final int LIFT_ENCODER_A_DIO_PORT = ;     if encoder in neos isn't good
+    private static final int LIFT_ENCODER_B_DIO_PORT = ;*/
+
+    public static DigitalInput liftLimitSwitchTop;
+    public static DigitalInput liftLimiSwitchtBot;
+
+    public CatzLift()
+    {
+        liftMtrCtrlLT = new CANSparkMax(LIFT_LT_MC_CAN_ID, MotorType.kBrushless);
+        liftMtrCtrlRT = new CANSparkMax(LIFT_RT_MC_CAN_ID, MotorType.kBrushless);
+        
+        liftMotors = new SpeedControllerGroup(liftMtrCtrlLT, liftMtrCtrlRT);
+       /* liftEnc = new Encoder(LIFT_ENCODER_A_DIO_PORT, 
+                              LIFT_ENCODER_B_DIO_PORT, false, EncodingType.k4X);*/
+    } 
+    public static void lift(double speed)
+    {
+        liftMotors.set(speed);
+    }
+    public static double getLiftCounts()
+    {
+        return liftMtrCtrlLT.getEncoder().getPosition();
+    }
+    public static boolean isLiftLimitSwitchTop()
+    {
+        return liftMtrCtrlLT.getForwardLimitSwitch(CANDigitalInput.LimitSwitchPolarity.kNormallyOpen).get(); 
+    }
+    public static boolean isLiftLimitSwitchBot()
+    {
+        return liftMtrCtrlRT.getForwardLimitSwitch(CANDigitalInput.LimitSwitchPolarity.kNormallyOpen).get();
+    }
+
+    public static void setLiftHeight(double targetCount, double speed) {
+
+            while(getLiftCounts()<targetCount) {
+                liftMotors.set(speed);
+            }
+        
+
+            while(getLiftCounts()>targetCount) {
+                liftMotors.set(-speed);
+            }
+    
+        liftMotors.set(0);
+
+    }
+
+
+
+
+  
+
+}
