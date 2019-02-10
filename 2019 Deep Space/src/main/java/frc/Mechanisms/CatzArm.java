10031c2b83f4b7e0043ef4b0a0b9c1d09ebacd91
unknown <hellen.kwon@gmail.com>
2019-02-09 17:23:45 -0800

    updated the lift and arm code 02/09/2019 JK

--- 2019 Deep Space/src/main/java/frc/Mechanisms/CatzArm.java
+++ 2019 Deep Space/src/main/java/frc/Mechanisms/CatzArm.java
@@ -1,11 +1,13 @@
 /*
  *  Author : Jean Kwon
-
+ * 
+ * unctionality : controls the armextension by the speed, controls the arm pivot by the speed,
+ *                  gets the status of each limit switch, gets the angle of the arm pivot,  
+ *                  moves the arm extension into the target distance, moves the arm pivot into the targetAngle
+ * 
  *  Methods : extension, pivot, getExtensionEncoderCounts, isArmLimitTipActivated, isArmLimitBaseActivated,
- *           getPivotAngle, setArmExtension
- *  Functionality : controls the armextension by the speed, controls the arm pivot by the speed,
- *                   gets the status of each limit switch, gets the angle of the arm pivot,  
- *                   sets the arm extension in to the target distance
+ *           getPivotAngle, moveArmExtension, moveArmPivot
+ * 
  *  Revision History : 
  *  02-04-19 Added the theread and the encoder JK
  * 
@@ -14,44 +16,60 @@ package frc.Mechanisms;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 
-
-
-public class CatzArm //static variables/objects
+public class CatzArm 
 {
-
-    private static WPI_TalonSRX armExtensionMtrCtrlA;
+    private static WPI_TalonSRX  armExtensionMtrCtrlA;  //A and B are designators
     private static WPI_VictorSPX armExtensionMtrCtrlB;
 
     private static CANSparkMax armPivotMtrCtrlLT;
     private static CANSparkMax armPivotMtrCtrlRT;
 
-    private final int ARM_EXTENSION_A_MC_ID = 20;
-    private final int ARM_EXTENSION_B_MC_ID = 21;
+    private final int ARM_EXTENSION_A_MC_CAN_ID = 20;
+    private final int ARM_EXTENSION_B_MC_CAN_ID = 21;
 
     private final int ARM_PIVOT_LT_MC_CAN_ID = 40;
     private final int ARM_PIVOT_RT_MC_CAN_ID = 41;
 
-    private static DigitalInput armExtensionLimitTip;
+    private static DigitalInput armExtensionLimitTip;   
     private static DigitalInput armExtensionLimitBase;
+
+    private final int ARM_EXTENSION_LIMIT_TIP_DIO_PORT  = 0; //TBD
+    private final int ARM_EXTENSION_LIMIT_BASE_DIO_PORT = 0; 
 
     private static AnalogInput armPivotEnc;
 
     private static final int ARM_PIVOT_ENCODER_ANALOG_PORT = 0; //TBD
-    
-    private static       double targetCount;
-    private static final int ARM_COUNT_THRESHOLD = 100; //TBD
-    private static final int ARM_COUNTS_PER_INCHES = 0; //TBD
+    private static final int ARM_PIVOT_ANGLE_TOLERANCE = 0; //TBD
+
+     /* **************************************************************************
+    * Arm Extension Encoder - pulses to inches 
+    * SRX Magnetic Encoder which provides 4096 pulses per revolution. 
+    * The gear reduction is 2 to 1.
+    * The diameter of wrench is 0.984 inch 
+    *****************************************************************************/
+
+    private static final double ARM_EXTENSION_ENCODER_PULSE_PER_REV = 4096;
+    private static final double ARM_EXTENSION_WINCH_DIAMETER = 0.984;
+    private static final double ARM_EXTENSION_GEAR_RATIO = 1/2;
+    private static final double ARM_COUNTS_PER_INCHES = ARM_EXTENSION_ENCODER_PULSE_PER_REV / 
+                                                       (ARM_EXTENSION_WINCH_DIAMETER * Math.PI) * ARM_EXTENSION_GEAR_RATIO ;
+
+    private static final double ARM_EXTENSION_COUNT_TOLERANCE = 100 * ARM_COUNTS_PER_INCHES; //TBD Type it in inches
+
+
 
     public CatzArm() {
 
-        armExtensionMtrCtrlA = new WPI_TalonSRX(ARM_EXTENSION_A_MC_ID);
-        armExtensionMtrCtrlB = new WPI_VictorSPX(ARM_EXTENSION_B_MC_ID);
+        armExtensionMtrCtrlA = new WPI_TalonSRX(ARM_EXTENSION_A_MC_CAN_ID);
+        armExtensionMtrCtrlB = new WPI_VictorSPX(ARM_EXTENSION_B_MC_CAN_ID);
 
         armExtensionMtrCtrlB.follow(armExtensionMtrCtrlA);
 
@@ -61,22 +79,22 @@ public class CatzArm //static variables/objects
         armPivotMtrCtrlLT.follow(armPivotMtrCtrlRT);
         //armPivotMtrCtrlLT.follow(armPivotMtrCtrlRT, true); if needs to be inverted
 
-        armExtensionLimitTip  = new DigitalInput(0); //TBD
-        armExtensionLimitBase = new DigitalInput(0); //TBD
+        armExtensionLimitTip  = new DigitalInput(ARM_EXTENSION_LIMIT_TIP_DIO_PORT); 
+        armExtensionLimitBase = new DigitalInput(ARM_EXTENSION_LIMIT_BASE_DIO_PORT); 
 
         armPivotEnc = new AnalogInput(ARM_PIVOT_ENCODER_ANALOG_PORT);
     }
 
-    public static void extension(double speed) {
-        armExtensionMtrCtrlA.set(speed);
+    public static void extension(double power) {
+        armExtensionMtrCtrlA.set(power);
     }
-    public static void pivot(double speed)
+    public static void pivot(double power)
     {
-        armPivotMtrCtrlRT.set(speed);
+        armPivotMtrCtrlRT.set(power);
     }
-    public static double getExtensionEncoderCounts()
+    public static int getExtensionEncoderCounts()
     {
-        return armExtensionMtrCtrlA.getSelectedSensorPosition();
+        return armExtensionMtrCtrlA.getSensorCollection().getQuadraturePosition();
     }
     public static boolean isArmLimitTipActivated()
     {
@@ -92,35 +110,84 @@ public class CatzArm //static variables/objects
         return (armPivotEnc.getVoltage()/5)*360;
     }
 
-    public static void setArmExtension(double trargetLength, double speed)
+    public static void moveArmExtension(double targetLength, double power, double timeOut)
     {
-        targetCount = trargetLength * ARM_COUNTS_PER_INCHES;
+        Timer threadTimer = new Timer();
+        threadTimer.start();
 
         Thread armExtensionThread = new Thread(() -> {
 
-            while (!Thread.interrupted()) 
-            {
-                while(getExtensionEncoderCounts() < targetCount-ARM_COUNT_THRESHOLD) 
-                {
-                    extension(speed);
-                }
-        
-                while(getExtensionEncoderCounts() > targetCount+ARM_COUNT_THRESHOLD)
-                {
-                    extension(-speed);
-                }
-        
-                armExtensionMtrCtrlA.stopMotor();
+                int   currentCount = getExtensionEncoderCounts();
+                double targetCount = targetLength * ARM_COUNTS_PER_INCHES;
 
-                if(targetCount-ARM_COUNT_THRESHOLD<getExtensionEncoderCounts()&&targetCount+ARM_COUNT_THRESHOLD>getExtensionEncoderCounts())
+                double upperLimit = targetCount + ARM_EXTENSION_COUNT_TOLERANCE;
+                double lowerLimit = targetCount - ARM_EXTENSION_COUNT_TOLERANCE;
+
+                if (currentCount < lowerLimit) 
                 {
-                    Thread.currentThread().interrupt();
+                    armExtensionMtrCtrlA.set(power);
+                } 
+                else if (currentCount > upperLimit)
+                {
+                    armExtensionMtrCtrlA.set(-power);
                 }
+                
+                while (!Thread.interrupted()) 
+                {
+                    currentCount = getExtensionEncoderCounts(); //update the arm extension current Count
+
+                    if((lowerLimit < currentCount && upperLimit > currentCount) || threadTimer.get()>timeOut)
+                    {
+                        armExtensionMtrCtrlA.stopMotor();
+                        Thread.currentThread().interrupt();
+                    }
+
+                    Timer.delay(0.005);
+
+                }
+            } );
+
+            armExtensionThread.start();
+       
+    }
+
+    public static void moveArmPivot(double targetAngle, double power, double timeOut) { //no more than 270 deg
+
+        Timer threadTimer = new Timer();
+        threadTimer.start();
+
+        Thread armPivotThread = new Thread(() ->
+        {
+            double currentAngle = getPivotAngle();
+
+            double upperLimit = targetAngle + ARM_PIVOT_ANGLE_TOLERANCE;
+            double lowerLimit = targetAngle - ARM_PIVOT_ANGLE_TOLERANCE;
+
+            if (targetAngle < 135) {  //270/2
+                armExtensionMtrCtrlA.set(power);
+            } else if(targetAngle > 135) {
+                armExtensionMtrCtrlA.set(-power);
             }
 
-        } );
+            while(!Thread.interrupted()) 
+            {
+                currentAngle = getPivotAngle(); //update the currentAngle
 
-        armExtensionThread.start();
-        
+                if((lowerLimit < currentAngle && upperLimit > currentAngle) || threadTimer.get()>timeOut) 
+                {
+
+                 armExtensionMtrCtrlA.stopMotor();
+                 Thread.currentThread().interrupt();
+
+                }
+
+                Timer.delay(0.005);
+            }
+       
+        });
+
+        armPivotThread.start();
+         
     }
+
 }
--- 2019 Deep Space/src/main/java/frc/Mechanisms/CatzLift.java
+++ 2019 Deep Space/src/main/java/frc/Mechanisms/CatzLift.java
@@ -16,7 +16,6 @@ package frc.Mechanisms;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Timer;
 
@@ -33,21 +32,20 @@ public class CatzLift
     private static final int LIFT_RT_MC_CAN_ID = 11; 
     private static final int LIFT_LT_MC_CAN_ID = 10;
 
-    private static final int LIFT_ENCODER_PULSE_PER_REV = 4096;
-    private static final int LIFT_WINCH_DIAMETER = 1;
-    private static final int LIFT_GEAR_RATIO = 1/6;
-
-    private static final double LIFT_COUNTS_PER_INCHES = LIFT_ENCODER_PULSE_PER_REV / 
-                                                         (Math.PI*LIFT_WINCH_DIAMETER) * LIFT_GEAR_RATIO;
-
-    /* **************************************************************************
-    * Drivetrain Encoder - pulses to inches 
+ /* **************************************************************************
+    * Lift Encoder - pulses to inches 
     * SRX Magnetic Encoder which provides 4096 pulses per revolution. 
     * The gear reduction is 6 to 1.
     * The diameter of wrench is 1 inch 
     *****************************************************************************/
 
-    private static final double LIFT_COUNT_TOLERANCE = 100 * LIFT_COUNTS_PER_INCHES; //TBD Type in inches
+    private static final double LIFT_ENCODER_PULSE_PER_REV = 4096;
+    private static final double LIFT_WINCH_DIAMETER = 1;
+    private static final double LIFT_GEAR_RATIO = 1/6;
+    private static final double LIFT_COUNTS_PER_INCHES = LIFT_ENCODER_PULSE_PER_REV / 
+                                                         (Math.PI*LIFT_WINCH_DIAMETER) * LIFT_GEAR_RATIO;
+
+    private static final double LIFT_COUNT_TOLERANCE = 100 * LIFT_COUNTS_PER_INCHES; //TBD Type it in inches
 
     /*public static Encoder liftEnc;              
     private static final int LIFT_ENCODER_A_DIO_PORT = ;     
@@ -62,7 +60,7 @@ public class CatzLift
         
         liftMotors = new SpeedControllerGroup(liftMtrCtrlLT, liftMtrCtrlRT);
         /*liftEnc = new Encoder(LIFT_ENCODER_A_DIO_PORT, 
-                              LIFT_ENCODER_B_DIO_PORT, false, EncodingType.k4X); */
+                                LIFT_ENCODER_B_DIO_PORT, false, EncodingType.k4X); */
     } 
 
     public static int getLiftCounts()
@@ -83,12 +81,11 @@ public class CatzLift
     public static void moveLift(double targetHeight, double power, double timeOut) 
     {      
         Timer threadTimer = new Timer();
-
         threadTimer.start();
 
         Thread liftThread = new Thread(() ->
         {
-           double currentCount = getLiftCounts();
+           int currentCount = getLiftCounts();
            double targetCount  = targetHeight * LIFT_COUNTS_PER_INCHES;
            
            double upperLimit = targetCount + LIFT_COUNT_TOLERANCE;
@@ -103,11 +100,11 @@ public class CatzLift
                 liftMotors.set(-power);
             }
 
-            while(!Thread.interrupted()) 
+            while(!Thread.interrupted())
             {
                 currentCount = getLiftCounts(); //update the current count of the lift
 
-                if((lowerLimit < currentCount && upperLimit > currentCount) || threadTimer.get()>timeOut) 
+                if((lowerLimit < currentCount && upperLimit > currentCount) || threadTimer.get() > timeOut) 
                 { 
                     liftMotors.stopMotor();
                     Thread.currentThread().interrupt();
